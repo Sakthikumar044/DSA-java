6882590c70ce84ec52751eb7736afdae73d1f82b
Sakthikumar044 <139200795+Sakthikumar044@users.noreply.github.com>
2023-07-11 20:42:46 +0530

    Add files via upload

---
+++ Day 2 greatest of two numbers .java
@@ -0,0 +1,16 @@
+public class Main
+ {
+   public static void main (String[]args)
+   {
+
+     int num1 = 59, num2 = 200;
+     if (num1 == num2)
+       System.out.println ("both are equal");
+     else if (num1 > num2)
+         System.out.println (num1 + " is greater");
+
+     else
+         System.out.println (num2 + " is greater");
+
+   }
+ }
